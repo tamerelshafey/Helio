5efb81acdcf8284904bf7240a40b0c66bbc354a8
tamerelshafey <tam.elshafey@gmail.com>
2025-11-19 13:32:01 +0200

    feat: Configure path aliases for services
    Introduces path aliases in `vite.config.ts` and `tsconfig.json` to simplify imports from the `services` directory. This change allows importing services using `api/*` instead of longer relative paths, improving code readability and maintainability. A dependency on the `path` module is also added to `index.html`.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
     define: {
         'process.env': {}
+    },
+    resolve: {
+        alias: [
+            { find: /^api\/(.*)/, replacement: path.resolve('services/$1') },
+            { find: /^\.\.\/api\/(.*)/, replacement: path.resolve('services/$1') },
+            { find: /^\.\.\/\.\.\/api\/(.*)/, replacement: path.resolve('services/$1') }
+        ]
     },
     build: {
         outDir: 'dist',
