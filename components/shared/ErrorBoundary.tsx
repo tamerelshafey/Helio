488d98e726e3c6d284767156c475562952be7ec6
tamerelshafey <tam.elshafey@gmail.com>
2025-11-28 19:17:46 +0200

    refactor: Update ErrorBoundary and locales
    Replaces React.Component with React.Component in ErrorBoundary.
    Resets locale files to empty objects.

--- components/shared/ErrorBoundary.tsx
+++ components/shared/ErrorBoundary.tsx
@@ -1,4 +1,4 @@
-import React, { Component, ErrorInfo, ReactNode } from 'react';
+import React, { ErrorInfo, ReactNode } from 'react';
 import { Button } from '../ui/Button';
 import { ExclamationCircleIcon } from '../ui/Icons';
 
@@ -12,7 +12,7 @@ interface State {
   error: Error | null;
 }
 
-class ErrorBoundary extends Component<Props, State> {
+class ErrorBoundary extends React.Component<Props, State> {
   public state: State = {
     hasError: false,
     error: null,
